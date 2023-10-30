ca4b4eb8b38925b3c886268f5f88183aa64a5279
Marcelo Conti <marceloconti9017@gmail.com>
2023-10-30 14:53:44 -0300

    adicionei a fonte nos textos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
-    <div>
+    <div class="container">
       <div class="header">
           <p>HOME</p>
           <p>INICIAIS</p>
           <p>SOBRE</p>
           <p>FORMULARIO</p>
       </div>
-      <div class="container">
-        <img class="neko" src="/img/neko-anotando.png" alt="logo ide"/>
+      <div class="titulo-imagem">
+        <img class="neko1" src="/img/neko-anotando.png" alt="logo ide"/>
         <div class="titulo">
           <p>POKE</p>
           <p><spam id="ide-vermelho">IDE</spam>X</p>
