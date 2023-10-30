47c4b0156ba36ab343dc82cca2f22b02723865ab
Marcelo Conti <marceloconti9017@gmail.com>
2023-10-30 00:35:03 -0300

    primeira versão da pagina 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <h1>
-      Olá, mundo!
-    </h1>
+    <div>
+      <div class="header">
+          <p>HOME</p>
+          <p>INICIAIS</p>
+          <p>SOBRE</p>
+          <p>FORMULARIO</p>
+      </div>
+      <div class="container">
+        <img class="neko" src="/img/neko-anotando.png" alt="logo ide"/>
+        <div class="titulo">
+          <p>POKE</p>
+          <p><spam id="ide-vermelho">IDE</spam>X</p>
+        </div>
+      </div>
+    </div>
   )
 }
