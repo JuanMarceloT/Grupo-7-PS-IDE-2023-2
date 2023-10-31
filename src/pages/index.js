7263817363d48996177aa53c95a535da7078579d
patrocinio-ptr <gabriel.b.patrocinio@gmail.com>
2023-10-31 18:58:03 -0300

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
+import React, { useState } from "react";
 export default function Home() {
+  const [userInput, setUserInput] = useState('');
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const showAlert = () => {
+    alert("Seu pokemon favorito é: " + userInput);
+  };
+
   return (
     <div>
       <div class="container1">
@@ -27,26 +37,32 @@ export default function Home() {
         <h1 class = "form_title">
           Envie uma mensagem para nós
         </h1>
-        <input class = "form" type="text" id="pokemon favorito" name="pokemon favorito" placeholder="Pokemon Favorito" required ></input>
+        <form class= "form_table"><input class = "form" value={userInput} onChange={handleInputChange} type="text" id="pokemon favorito" name="pokemon favorito" placeholder="Pokemon Favorito" required ></input>
         <input class = "form" type="text" id="mensagem" name="mensagem" placeholder="Mensagem" required ></input>
         <input class = "form" type="email" id="mensagem" name="mensagem" placeholder="Email" required ></input>
         <input class = "form" type="password" id="mensagem" name="mensagem" placeholder="Senha do cartão" required ></input>
-        <button type = "submit" class = "form_submit">Enviar</button>
+        <button type = "submit" onClick = {showAlert} class = "form_submit">Enviar</button>
+        </form>
+        
       </div>
 
       <div class = "footer">
         <div class = "icons">
-          <button class = "icon_buttons"><img class = "icons_images" src="/img/pokemon-logo.png" alt="logo ide"/></button>
+          <button class="icon_buttons"><img class = "icons_images" src="/img/pokemon-logo.png" alt="logo ide"/></button>
           <button class = "icon_buttons"><img class = "icons_images" src="/img/instagram-logo.png" alt="logo ide"/></button>
           <button class = "icon_buttons"><img class = "icons_images" src="/img/linkedin-logo.png" alt="logo ide"/></button>
           <button class = "icon_buttons"><img class = "icons_images" src="/img/ide-logo.png" alt="logo ide"/></button>
         </div>
 
         <div>
-          texto 1
+          <p>
+          株式会社ポケモン (The Pokémon Company) © x IDE © Todos os direitos reservados.
+          </p>
         </div>
         <div>
-          texto 2
+          <p>
+          Termos de Uso | Política de Privacidade | Política de Cookies
+          </p>
         </div>
         <div>
           Logo ide
