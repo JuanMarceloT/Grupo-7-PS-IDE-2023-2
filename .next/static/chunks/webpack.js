265e2f5ce67fd30a345896c001511a4f392be2c0
patrocinio-ptr <gabriel.b.patrocinio@gmail.com>
2023-10-31 20:39:13 -0300

    Icones flutuantes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b02c57708c08b65"; }
+/******/ 		__webpack_require__.h = function() { return "2453327524c99a92"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
