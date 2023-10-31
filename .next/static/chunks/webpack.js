416d77f4404e50494fb8f4a5d65246ec2f983c7f
Marcelo Conti <marceloconti9017@gmail.com>
2023-10-31 19:35:25 -0300

    coloquei transição smooth

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0523c7e60758f2a7"; }
+/******/ 		__webpack_require__.h = function() { return "563cec8adb3c55f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
