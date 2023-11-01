4487e82304954f081e1a91ae8da515bb5ee9177f
Marcelo Conti <marceloconti9017@gmail.com>
2023-11-01 20:42:34 -0300

    ajuste na sombra dos textos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9c3c99a12560bfe"; }
+/******/ 		__webpack_require__.h = function() { return "1b9699e41e073cd5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
